eb44328ac518601eabd25964f228bf48e761dbb2
ChenXuzheng <109289706@qq.com>
2021-10-28 00:15:26 +0800

    init the struct

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # zju_bs2021_frontend
+
 the frontend source code for zju bs course 2021, by react and ant design pro
+
+
+
+A website for image data annotation for machine learning
---
+++ config/defaultSettings.js
@@ -0,0 +1,12 @@
+const Settings = {
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  primaryColor: '#722ED1',
+  navTheme: 'light',
+  layout: 'side',
+  title: 'Annotation OL',
+  splitMenus: false,
+  fixSiderbar: true,
+  logo: 'https://i.loli.net/2021/10/27/kJWcOx3RA6GwFEV.jpg',
+};
+export default Settings;
